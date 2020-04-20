ca0fa91aab5133f45108bd09fe8495e33e277ffd
alfianhidayahh <62759588+alfianhidayahh@users.noreply.github.com>
2020-04-21 01:32:01 +0700

    patch 1

---
+++ app/src/main/java/com/example/exercisesqlite/profile.java
@@ -0,0 +1,16 @@
+package com.example.exercisesqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class profile extends AppCompatActivity {
+    private ListView obj;
+    DBHelper mydb;
+
+
+}
